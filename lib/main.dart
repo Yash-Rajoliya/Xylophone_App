84b0e25eeae647c5c43aa4bd3894d66514bf4612
Dell <yashrajoliya@gmail.com>
2021-10-06 15:37:23 +0530

    Xylophone

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
+
+void playSound (int soundNumber ){
+  final player = AudioCache();
+  player.play('note$soundNumber.wav');
+}
+
+Expanded xylophone_key ({Color color , int soundNumber , int noteNumber }){
+   return Expanded(
+    child: FlatButton(
+      padding: EdgeInsets.symmetric(vertical: 0.0,horizontal: 0.0),
+      color: color,
+      onPressed: (){
+        playSound(soundNumber);
+      },
+      child: Text('Note $noteNumber', style: TextStyle(
+          color: Colors.white
+      ),),
+    ),
+  );
+}
 
 class XylophoneApp extends StatelessWidget {
   @override
@@ -8,7 +29,19 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              xylophone_key(color: Colors.red,soundNumber: 1,noteNumber : 1 ),
+              xylophone_key(color: Colors.orange,soundNumber: 2,noteNumber : 2 ),
+              xylophone_key(color: Colors.yellow,soundNumber: 3,noteNumber : 3 ),
+              xylophone_key(color: Colors.green,soundNumber: 4,noteNumber : 4 ),
+              xylophone_key(color: Colors.teal,soundNumber: 5,noteNumber : 5 ),
+              xylophone_key(color: Colors.blue,soundNumber: 6,noteNumber : 6 ),
+              xylophone_key(color: Colors.purple,soundNumber: 7,noteNumber : 7 ),
+            ],
+          )
         ),
       ),
     );
